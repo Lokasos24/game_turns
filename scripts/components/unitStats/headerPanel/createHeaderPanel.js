3537d91d957acc70b92acdb1ab7d62709e8b6f4b
Lokasos24 <tatan.lps@hotmail.com>
2026-04-30 12:38:18 -0400

    ADD: Se puede visualizar ahora el enemigo en el componente de HeaderPanel

--- scripts/components/unitStats/headerPanel/createHeaderPanel.js
+++ scripts/components/unitStats/headerPanel/createHeaderPanel.js
@@ -1,20 +1,22 @@
 import { captureUnit } from "../../../utils/helpers.js"
+import {getEnemie} from "../../../services/enemies/captureEnemie.js"
 
 export function createHeaderPanel(gameState, headerPanel) {
     headerPanel.replaceChildren()
 
-    const changes = captureUnit(gameState)
+    const changes = captureUnit(gameState) || getEnemie(gameState)
 
     if (!changes) return
 
     const createSpanName = headerPanel.appendChild(document.createElement('span'))
     createSpanName.className = `unit-name`
-    createSpanName.textContent = `${changes.name}`
+    createSpanName.textContent = `${changes.name || changes.type}`
 
     const createLevelUnit = headerPanel.appendChild(document.createElement('span'))
     createLevelUnit.className = `unit-level`
-    createLevelUnit.textContent = `Lv. ${changes.level}`
+    createLevelUnit.textContent = `lvl ${changes.level}`
 
+    if(!changes.class || !changes.kind) return
     const createClassUnit = headerPanel.appendChild(document.createElement('span'))
     createClassUnit.className = `unit-class`
     createClassUnit.textContent = `${changes.class || changes.kind}`
